c7e13bf1980a44cf1a1de9dc2c5c4e029f8c6687
KingMastana <40878648+KingMastana@users.noreply.github.com>
2025-05-22 23:56:09 +0000

    Learn and implement form submit events

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,17 @@
+import type { FormEvent } from "react";
+
 export default function Main() {
   const ingredients = ["orange", "mango", "apple"];
 
   const ingredientsListItems = ingredients.map((ingredient) => (
     <li key={ingredient}>{ingredient}</li>
   ));
-  function handleSubmit() {
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
     console.log("Form submitted!");
+    const formData = new FormData(event.currentTarget)
+    const newIngredient = formData.get("ingredient")
+    console.log(newIngredient)
   }
   return (
     <main>
@@ -14,6 +20,7 @@ export default function Main() {
           type="text"
           placeholder="e.g. oregano"
           aria-label="Add ingredient"
+          name="ingredient"
         />
         <button>Add ingredient</button>
       </form>
